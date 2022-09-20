0f29e818959bfc26bd13317235e9becbf27928fb
Evgeniy <75331348+y0nkers@users.noreply.github.com>
2022-09-20 21:01:30 +0300

    - Added the ability to select a preferred value. - Added various checks for the correct operation of the program

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # DOTA-Camera-Change-Distance
-### Simple python script for changing camera distance in DOTA2 to the optimal value from 1200 to 1500
+### Simple python script for changing camera distance in DOTA2 to the optimal value from 1200 to 1600
 
 # How to use
 - ### Download .exe file from [release page](https://github.com/y0nkers/DOTA-Camera-Change-Distance/releases)
 - ### Drop it in in one folder with client.dll (dota 2 beta\game\dota\bin\win64)
 - ### Execute file
+- ### Enter your preferred camera distance and press Enter
 
 # Screenshots
 ### dota_camera_distance = 1200 (default)
 
 ![default](https://user-images.githubusercontent.com/75331348/191089125-1f937744-84e9-457f-8e60-ecbcb4b00098.png)
 
-### dota_camera_distance = 1500 (changed)
+### dota_camera_distance = 1600 (changed)
 
 ![image](https://user-images.githubusercontent.com/75331348/191089257-8d691158-7265-4c1a-bd20-a5361bc17eb7.png)
-
-# Why 1500?
-### 1500 is the most optimal value. Values above 1500 result in black areas where the image is not rendered and where mouse clicks are not processed.
--- main.py
+++ main.py
@@ -1,12 +1,36 @@
+import struct
+
 if __name__ == '__main__':
-    distance_1200 = "0000964400409c440080a2440080b444"
-    distance_1500 = "0080bb4400409c440080a2440080b444"
-    distance_1200_bytes = bytes.fromhex(distance_1200)
-    distance_1500_bytes = bytes.fromhex(distance_1500)
+    while True:
+        try:
+            distance = int(input("Enter your preferred camera distance (value must be between 1200 and 1600): "))
+            assert 1200 <= distance <= 1600
+            break
+        except (ValueError, AssertionError):
+            print("Camera distance value must be an integer between 1200 and 1600")
+
+    # Converting distance from decimal to hexadecimal value
+    distance_hex = hex(struct.unpack('<I', struct.pack('<f', distance))[0])[2:]
+    # Reverse bytes of hex value (because this is how it's written in config.dll)
+    hex_reversed = "".join([distance_hex[x:x + 2] for x in range(0, len(distance_hex), 2)][::-1])
+
+    default = "0000964400409c440080a2440080b444"  # 1200 as hex value
+    new = hex_reversed + "00409c440080a2440080b444"
+    default_bytes = bytes.fromhex(default)
+    new_bytes = bytes.fromhex(new)
     path = "client.dll"
 
-    with open(path, "r+b") as file:
-        data = file.read()
-        offset = data.find(distance_1200_bytes)
-        file.seek(offset)
-        file.write(distance_1500_bytes)
+    try:
+        with open(path, "r+b") as file:
+            data = file.read()
+            offset = data.find(default_bytes)
+            file.seek(offset)
+            file.write(new_bytes)
+
+        print("The new value is set to " + str(distance))
+        input("Press Enter to close this window...")
+    except FileNotFoundError:
+        print("Client.dll could not be found. Place this .exe file in "
+              "the same directory as client.dll (dota 2 beta\\game\\dota\\bin\\win64)")
+    except OSError:
+        print("Error while reading data. The client.dll file may have already been modified.")
